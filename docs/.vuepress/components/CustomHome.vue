9fd5d1e3c706467782d706e6daccc145722f775a
emmecollective <131177275+emmecollective@users.noreply.github.com>
2023-04-19 00:07:20 +0900

    Add files via upload

---
+++ docs/.vuepress/components/CustomHome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HomeContent from '@vuepress/theme-default/lib/client/components/HomeContent.vue'
+import HomeFeatures from '@vuepress/theme-default/lib/client/components/HomeFeatures.vue'
+import HomeFooter from '@vuepress/theme-default/lib/client/components/HomeFooter.vue'
+import HomeHero from '@vuepress/theme-default/lib/client/components/HomeHero.vue'
+</script>
+
+<template>
+  <main class="home">
+    <HomeHero />
+    <HomeFeatures />
+    <HomeContent />
+    <HomeFooter />
+  </main>
+</template>
